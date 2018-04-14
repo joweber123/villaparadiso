b4f67154ab733b0404060ee711bdf5d4caee98f4
James Weber <joweber123@gmail.com>
2018-04-14 09:40:51 +0200

    fixed hamburger closing bars

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,13 @@
-var el = document.querySelector('.hamburger-icon');
+var hamburger = document.querySelector('.hamburger-icon');
 var nav = document.querySelector('.main-nav');
 var lang = document.querySelector('.language-picker');
+var bar = document.getElementsByClassName('bar');
 
-el.onclick=function(){
+hamburger.onclick=function(){
   nav.classList.toggle('js-remove-top-position');
   lang.classList.toggle('js-remove-top-position');
+  var i;
+  for (i = 0; i< bar.length; i++){
+    bar[i].classList.toggle('open');
+  }
 };
